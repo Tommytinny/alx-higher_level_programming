d3ee64ea113e45e2fff332683d3d576254f97151
Tommytinny <tommyteenboi@gmail.com>
2023-10-06 02:16:16 +0100

    added j

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    
+
     names = dir(hidden_4)
     sign = "__"
     start = True
-    
+
     for i in names:
         string = i
         for j in range(len(sign)):
