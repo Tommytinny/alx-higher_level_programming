528f7bae1478e861942ff3b6d88b7f8989641843
Tommytinny <tommyteenboi@gmail.com>
2023-10-06 01:27:36 +0100

    file fixed now

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("1 + 2 = {}".format(int(add(a, b))))
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
