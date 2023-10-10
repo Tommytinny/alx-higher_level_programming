1c3b5c0a84354f2405f46cc4214bcdb034beceb8
Tommytinny <tommyteenboi@gmail.com>
2023-10-10 01:04:11 +0100

    fixed errors in each files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        n_list = []
-        for i in range(len(my_list)):
-            n_list = n_list + [my_list[i]]
-        return n_list
+    new_list = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return new_list
     else:
-        n_list = []
-        for i in range(len(my_list)):
-            if i == idx:
-                n_list = n_list + [element]
-            else:
-                n_list = n_list + [my_list[i]]
-            return n_list
+        new_list[idx] = element
+        return new_list
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        j = 0
-        for row in matrix:
-            i = 0
-            for element in row:
-                print("{:d} ".format(matrix[j][i]), end="")
-                i = i + 1
-            print()
-            j = j + 1
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
+        print()
