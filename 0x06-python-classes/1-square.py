b7cb055cc81e3753ed2f87adc487908f731e5844
Tommytinny <tommyteenboi@gmail.com>
2023-10-26 01:34:35 +0100

    push files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define a class named Square """
+
+
+class Square:
+    """ Define a function __init__ method """
+    def __init__(self, size=None):
+        """ initializes __size of self with size """
+        self.__size = size
