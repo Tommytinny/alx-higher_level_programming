e54e45cb11c31cd35889f63a0a0de134c9f36161
Tommytinny <tommyteenboi@gmail.com>
2023-10-31 21:29:58 +0100

    fixed

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""
+Module that defines a rectangle
+"""
+
 
 class Rectangle:
-
+    """ Represents a rectangle """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """ initialize """
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
+        """ width getter """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -20,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ height getter """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ height setter """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -31,24 +40,29 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """ calculate area of a rectangle """
         return (self.__height * self.__width)
 
     def perimeter(self):
+        """ calculate perimeter of a rectangle """
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * (self.__height + self.__width))
 
     def __str__(self):
+        """ return set of rectangle """
         if self.__width == 0 or self.__height == 0:
             return ''
         hashes = ""
-        while self.__height != 0:
-            if (self.__height - 1) != 0:
+        height = self.__height
+        while height != 0:
+            if (height - 1) != 0:
                 hashes += "#" * self.__width + "\n"
             else:
                 hashes += "#" * self.__width
-            self.__height -= 1
+            height -= 1
         return hashes
 
     def __repr__(self):
+        """ repr """
         return "Rectangle({}, {})".format(self.__width, self.__height)
