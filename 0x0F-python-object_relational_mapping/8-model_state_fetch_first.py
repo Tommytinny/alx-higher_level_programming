7d954850e94470040cbe9d3bb3ee3db1afa9261c
Tommytinny <tommyteenboi@gmail.com>
2024-01-16 11:24:10 -0500

    added files

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     if first_state:
         print('{}: {}'.format(first_state.id, first_state.name))
     else:
-        print("")
+        print("Nothing")
 
     session.close()
