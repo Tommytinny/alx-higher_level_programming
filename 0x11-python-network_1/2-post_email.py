de1b71d7f52ed3bd56d133f47d758af36b19629f
Tommytinny <tommyteenboi@gmail.com>
2024-02-06 17:12:41 -0500

    2-post_email.py: removed trailing space and added required blank lines

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST 
-request to the passed URL with the email as a parameter, and 
+"""Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)
 """
 import urllib.request
 import urllib.parse
 from sys import argv
+
 
 def main():
     """ main function """
