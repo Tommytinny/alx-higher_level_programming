fbe8276391eadc1b0b97236410e734bcc1cdb35a
Tommytinny <tommyteenboi@gmail.com>
2024-02-06 17:33:29 -0500

    4-hbtn_status.py: pycodestyle standard

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request 
+"""Python script that takes in a URL, sends a request
 to the URL and displays the value of the variable
-X-Request-Id in the response header"""
+X-Request-Id in the response header
+"""
 import requests
 from sys import argv
+
 
 def main():
     """ main function """
     r = requests.get(argv[1])
-    page = r.headers['X-Request-Id']
-    print(page)
+    for k in r.headers:
+        if k == 'X-Request-Id':
+            page = r.headers['X-Request-Id']
+            print(page)
+            break
 
 
 if __name__ == '__main__':
