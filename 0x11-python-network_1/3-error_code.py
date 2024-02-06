bb7500b6097c3498c699e7219bf0442f280feea7
Tommytinny <tommyteenboi@gmail.com>
2024-02-06 17:17:06 -0500

    3-error_code.py: added module documentation

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""  Python script that takes in a URL, sends
+a request to the URL and displays the body 
+of the response (decoded in utf-8)
+"""
 import urllib.request
 import urllib.error
 from sys import argv
 
+
 def main():
+    """ main function """
     req = urllib.request.Request(argv[1])
     try:
         with urllib.request.urlopen(req) as response:
