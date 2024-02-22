96573e1bbbd76649779712c4e4bc5ae09bd7e0bb
Tommytinny <tommyteenboi@gmail.com>
2024-02-21 23:50:15 -0500

    added all finished files

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,13 +2,13 @@
 
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-const urlFinal = url.concat('/${process.argv[2]}/');
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const urlFinal = url.concat(`/${process.argv[2]}/`);
 
 request(urlFinal, (error, response, body) => {
   const data = JSON.parse(body);
   if (error) {
-    console.error(errorC);
+    console.error(error);
   }
   for (const element of data.characters) {
     request(element, (errorC, responseC, bodyC) => {
