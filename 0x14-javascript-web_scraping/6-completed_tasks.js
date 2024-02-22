b50dbd01042513ceedd3ea0c1855753e11defb5a
Tommytinny <tommyteenboi@gmail.com>
2024-02-21 23:47:00 -0500

    added all finished files

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,26 +3,23 @@
 const request = require('request');
 
 const url = process.argv[2];
-const completedDict = {}
-
+const completedDict = {};
 
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   }
   const data = JSON.parse(body);
-    for (const element of data) {
-      if (!(element.userId in completedDict)) {
-        if (element.completed === true) {
-	  completedDict[element.userId] = 1;
-			
-	}
-		
-      } else {
-	if (element.completed === true) {
-		completedDict[element.userId] = completedDict[element.userId] + 1;
-	}
+  for (const element of data) {
+    if (!(element.userId in completedDict)) {
+      if (element.completed === true) {
+        completedDict[element.userId] = 1;
+      }
+    } else {
+      if (element.completed === true) {
+        completedDict[element.userId] = completedDict[element.userId] + 1;
       }
     }
-    console.log(completedDict);
+  }
+  console.log(completedDict);
 });
