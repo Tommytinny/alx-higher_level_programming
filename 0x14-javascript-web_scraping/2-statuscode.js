4f4f4bb1301e7b481993f46ea830e0f30be7f3b5
Tommytinny <tommyteenboi@gmail.com>
2024-02-21 23:37:22 -0500

    added all finished files

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,8 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (error, response, body) => {
-  console.log('code: ${response.statusCode}');
+  console.log(`code: ${response.statusCode}`);
   if (error) {
-    console.error('code: ${response.statusCode)');
+    console.error(`code: ${response.statusCode}`);
   }
 });
